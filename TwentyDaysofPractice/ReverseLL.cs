79d50d6a1c563a2a7fb32fd217f35d70dcef0979
Gary Smith <ississippi@outlook.com>
2025-02-23 13:47:02 -0500

    Reverse linked list

---
+++ TwentyDaysofPractice/ReverseLL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyDaysofPractice
+{
+    internal class ReverseLL
+    {
+        public static Node Reverse(Node head)
+        {
+            if (head == null || head.next == null)
+                return head;
+
+            var first = head;
+            var second = first.next;
+
+            while(second != null)
+            {
+                var temp = second.next;
+                second.next = first;
+                first = second;
+                second = temp;
+            }
+
+            head.next = null;
+            head = first;
+
+            return head;
+
+        }
+    }
+}
